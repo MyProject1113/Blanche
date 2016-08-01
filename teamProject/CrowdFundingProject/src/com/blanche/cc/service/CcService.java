997e508e9886eca4fb57e975f9546154592f7000
loliking <sonkunyul@gmail.com>
2016-08-01 17:37:45 +0900

    [loliking] cc update

---
+++ teamProject/CrowdFundingProject/src/com/blanche/cc/service/CcService.java
@@ -0,0 +1,14 @@
+package com.blanche.cc.service;
+
+/*import java.util.List;
+
+import com.blanche.cc.vo.CcVO;
+*/
+public interface CcService {
+/*	public List<CcVO> ccList();
+	public int ccInsert(CcVO bvo);
+	public CcVO ccDetail(CcVO bvo);
+	public int pwdConfirm(CcVO bvo);
+	public int ccUpdate(CcVO bvo);
+	public int ccDelete(int b_num);*/
+}
